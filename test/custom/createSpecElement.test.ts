41b4dc21068e311a75aac11528acee8e47c92ec3
YizYah <yyakovson613@gmail.com>
2021-03-21 15:20:57 +0200

    docs(readme): added gif and usage.  Also description

--- test/custom/createSpecElement.test.ts
+++ test/custom/createSpecElement.test.ts
@@ -71,33 +71,3 @@ test('createSpecElement', async t => {
     "slow": true,
   })
 });
-
-// const secondConfig={
-//   "noSession": {
-//     "type": "string",
-//   },
-//   "nothing": {
-//     "type": "string",
-//     "required": "true",
-//   }
-// }
-//
-// test('no session passed in', async t => {
-//   const userAnswers = [
-//     'blah', // noSession
-//     '', // nothing, first time
-//     'foo', // nothing, second time
-//   ]
-//   await userSession(userAnswers)
-//   stdout.start()
-//   const output = await createSpecElement(secondConfig) // no session
-//   stdout.stop()
-//   t.regex(stdout.output, /nothing is required. Please enter value/)
-//   //make sure prompting fpr a new value after none is entered
-//
-//   t.deepEqual(output, {
-//     "noSession":"blah",
-//     'nothing': "foo",
-//   })
-// });
-//
